8d9371d30aef48123e2cc2b6a9a3dbd2ef751f77
Jiri Danek <jdanek@redhat.com>
2016-05-18 13:39:59 +0200

    solved 25

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -2,6 +2,28 @@ package iii_conventions
 
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
 
+operator fun MyDate.compareTo(other: MyDate): Int {
+    if (year < other.year) {
+        return -1
+    } else if (year > other.year) {
+        return 1
+    }
+
+    if (month < other.month) {
+        return -1
+    } else if (month > other.month) {
+        return 1
+    }
+
+    if (dayOfMonth < other.dayOfMonth) {
+        return -1
+    } else if (dayOfMonth > other.dayOfMonth) {
+        return 1
+    }
+
+    return 0
+}
+
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
 enum class TimeInterval {
